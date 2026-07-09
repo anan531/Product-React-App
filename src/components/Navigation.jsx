0ea86be8a06432d55b0d8167b102b02994f5d80a
anan531 <ananyat612@gmail.com>
2026-07-09 14:51:38 +0530

    Completed

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Product App</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">AddProduct</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/delete">DeleteProduct</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/search">SearchProduct</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/view">ViewProduct</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    </div>
+  )
+}
+
+export default Navigation
---
+++ src/components/ViewProduct.jsx
@@ -0,0 +1,145 @@
+import React from 'react'
+import Navigation from './Navigation'
+
+const ViewProduct = () => {
+  return (
+    <div>
+<Navigation />
+<div className="container">
+    <div className="row g-3">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<div className="container">
+    <div className="row g-3">
+        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card">
+  <img src="https://www.sotrue.in/cdn/shop/files/00000001.jpg?v=1756963184&width=1080" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">PRIMER</h5>
+    <p class="card-text">PRICE:$35</p>
+    <a href="#" class="btn btn-secondary">ADD TO CART</a>
+  </div>
+</div>
+
+        </div>
+        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiEOPKOppyujZoHSEK-Iu20UchLEefyLwhb4no0qQEDQ&s=10" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">SUNSCREEN</h5>
+    <p class="card-text">PRICE:$35</p>
+    <a href="#" class="btn btn-secondary">ADD TO CART</a>
+        </div>
+        </div>
+        </div>
+        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO6VlpMdwSGuuiCAXHW4UEw42fs9uxFgCC5Q4VsL7A4w&s=10" class="card-img-top" height="410" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">FOUNDATION</h5>
+    <p class="card-text">PRICE:$35</p>
+    <a href="#" class="btn btn-secondary">ADD TO CART</a>
+        </div>
+        </div>
+
+        </div>
+        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuDM4mPRzTuMRH1p3FjKBU3MG6Vu2NGWUe-48Vz1ATpg&s=10" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">CONTOUR</h5>
+    <p class="card-text">PRICE:$35</p>
+    <a href="#" class="btn btn-secondary">ADD TO CART</a>
+        </div>
+        </div>
+
+        </div>
+        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnLMsOhYHbj7x-koVM5rCES2_Js1qa_Sf9sElAIum8-w&s=10" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">BLUSH</h5>
+    <p class="card-text">PRICE:$35</p>
+    <a href="#" class="btn btn-secondary">ADD TO CART</a>
+        </div>
+        </div>
+
+        </div>
+        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+
+<div class="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8qlI1_JNH5oBXkYExpdj_bXzrIAMjCRQR7TJob9j-cg&s=10" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">LIPSTICK</h5>
+    <p class="card-text">PRICE:$35</p>
+    <a href="#" class="btn btn-secondary">ADD TO CART</a>
+        </div>
+        </div>
+
+        </div>
+        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card">
+  <img src="https://www.sotrue.in/cdn/shop/files/00000001.jpg?v=1756963184&width=1080" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">PRIMER</h5>
+    <p class="card-text">PRICE:$35</p>
+    <a href="#" class="btn btn-secondary">ADD TO CART</a>
+        </div>
+        </div>
+
+        </div>
+        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card">
+  <img src="https://www.sotrue.in/cdn/shop/files/00000001.jpg?v=1756963184&width=1080" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">PRIMER</h5>
+    <p class="card-text">PRICE:$35</p>
+    <a href="#" class="btn btn-secondary">ADD TO CART</a>
+        </div>
+        </div>
+
+        </div>
+        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card">
+  <img src="https://www.sotrue.in/cdn/shop/files/00000001.jpg?v=1756963184&width=1080" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">PRIMER</h5>
+    <p class="card-text">PRICE:$35</p>
+    <a href="#" class="btn btn-secondary">ADD TO CART</a>
+        </div>
+        </div>
+
+        </div>
+        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+<div class="card">
+  <img src="https://www.sotrue.in/cdn/shop/files/00000001.jpg?v=1756963184&width=1080" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">PRIMER</h5>
+    <p class="card-text">PRICE:$35</p>
+    <a href="#" class="btn btn-secondary">ADD TO CART</a>
+        </div>
+        </div>
+
+        </div>
+    </div>
+</div>
+
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default ViewProduct
